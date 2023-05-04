7a7f97870f6f87f1f4ea64448836e2c940729f00
Sergii Kirianov <s.kirianov@gmail.com>
2023-05-04 12:29:10 +0200

    fix: added no routes print out if no routes are found

--- src/components/LeftDrawer.tsx
+++ src/components/LeftDrawer.tsx
@@ -3,12 +3,15 @@ import Link from 'next/link';
 export const LeftDrawer = ({ methodRoutes }: any) => {
   return (
     <div className="w-96 bg-green-900 rounded-md fixed top-0 left-0 h-screen">
-      {methodRoutes.length &&
+      {methodRoutes.length ? (
         methodRoutes.map((route: any) => (
           <Link href={route.path} key={route.name}>
             {route.name}
           </Link>
-        ))}
+        ))
+      ) : (
+      <div className="text-white">No routes</div>
+      )}
     </div>
   );
 };
